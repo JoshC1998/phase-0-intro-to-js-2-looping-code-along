8b90061e48d5528eac264fff54ce0fde669fa722
Joshua Cruz <jcruz1998@gmail.com>
2024-05-22 14:02:33 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+let names = ['Alice', 'Bob', 'Charlie'];
+let giftName = 'oreo';
+
+let result = writeCards(names, eventName);
+console.log(result);
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
